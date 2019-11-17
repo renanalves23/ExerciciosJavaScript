401ae29ae8a160786d2c42331a78cabe42c23b86
renan <renanalves23@outlook.com>
2019-11-17 13:17:41 -0300

    2 métodos criados e os resultados lançados no HTML

--- js/ex2.js
+++ js/ex2.js
@@ -73,10 +73,14 @@ console.log(novoArray);
                 'n_aval_2_estrelas': 20,
                 'n_aval_1_estrela': 4,
                 'total_aval': function () {
-                  let t_aval = 'n_aval_5_estrelas' + 'n_aval_4_estrelas' + 'n_aval_3_estrelas' + 'n_aval_2_estrelas' + 'n_aval_1_estrela';
+                  const t_aval = this.n_aval_5_estrelas + this.n_aval_4_estrelas + this.n_aval_3_estrelas + this.n_aval_2_estrelas + this.n_aval_1_estrela;
                   return t_aval;
                 },
-                
+                'media_aval': function () {
+                  //média ponderada
+                  const m_aval = ((5 * this.n_aval_5_estrelas) + (4 * this.n_aval_4_estrelas) + (3 * this.n_aval_3_estrelas) + (2 * this.n_aval_4_estrelas) + (1 * this.n_aval_3_estrelas) / (this.n_aval_5_estrelas + this.n_aval_4_estrelas + this.n_aval_3_estrelas + this.n_aval_2_estrelas + this.n_aval_1_estrela));
+                  return m_aval;
+                } 
             };
         /*
          a) A categoria principal do curso é o primeiro elemento da lista associada à chave 'categoria'.
@@ -91,9 +95,16 @@ console.log(novoArray);
              /* b) As propriedades n_aval... representam a quantidade de avaliações do curso, de 1 até 5 estrelas. 
              Crie dois métodos: um para retornar o total de avaliações do curso e um para retornar a média de estrelas que 
              tem o curso (exemplo: 4.5). Depois utilize-os para soltar estas informações nas caixas abaixo, que tem os ids 
-             "total_aval" e "media_aval". */
+             "total_aval" e "media_aval". 
              
-        
+             ( (valor1 * peso 1) + ( valor 2 * peso 2) + ... ) / ( somatório dos pesos )*/
+      
+      const total_av = curso.total_aval();
+      const media_av = curso.media_aval().toFixed(2); //2 casas decimais
+
+      document.getElementById("media_aval").innerHTML = media_av
+
+      document.getElementById("total_aval").innerHTML = total_av;
 
  console.log(`---------------------------------------`)
 
